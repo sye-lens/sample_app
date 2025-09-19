5e02364ea6ca02f77b72f2c6652e497e17e3e9fe
sye-lens <rcp655317@g,amail.com>
2025-09-19 22:01:13 +0900

    Finish layout and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,5 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal "Ruby on Rails Tutorial Sample App", full_title
     assert_equal "Help | Ruby on Rails Tutorial Sample App", full_title("Help")
+    assert_equal "Sign up | Ruby on Rails Tutorial Sample App", full_title("Sign up")
   end
 end
